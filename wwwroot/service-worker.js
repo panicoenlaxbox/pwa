4ef2dccd6b9520aa345523e060e7e85c3ba22c8e
panicoenlaxbox <panicoenlaxbox@gmail.com>
2020-04-23 16:13:40 +0200

    Service worker lifecycle

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,9 +1,34 @@
-self.addEventListener('install', event => {
+const cacheName = "v8";
+
+self.addEventListener('install', e => {
+    console.log('install');
+    // self.skipWaiting();
+    const preCache = async () => {
+        let cache = await caches.open(cacheName);
+        return cache.addAll([
+            'long-task?delay=2000',
+        ]);
+    };
+    e.waitUntil(preCache());
+});
+
+self.addEventListener('activate', e => {
+    console.log('activate');
+    // self.clients.claim();
 });
 
 self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            })
+        ))
+    );
 });
 
-self.addEventListener('fetch', function (event) {
-    console.log('fetch', event.request.url);
+self.addEventListener('fetch', function (e) {
+    console.log('fetch', e.request.url);
 });
